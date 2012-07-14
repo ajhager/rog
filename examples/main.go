b5838e3ce503c8ccd54bc29ff91a9bb83b8bab09
Joseph Hager <ajhager@gmail.com>
2012-07-14 15:11:34 -0400

    Make example use Console.Width() and .Height()

--- examples/main.go
+++ examples/main.go
@@ -7,15 +7,15 @@ import (
 
 func game(w *rog.Window) {
 	grey := color.RGBA{20, 20, 20, 255}
-	for x := 0; x < 40; x++ {
-		for y := 0; y < 20; y++ {
-			purple := color.RGBA{uint8(150 + x%255), uint8(y * x), uint8((y * 4) % 255), 255}
+	for x := 0; x < w.Width(); x++ {
+		for y := 0; y < w.Height(); y++ {
+			purple := color.RGBA{uint8(200), uint8(y * x), uint8((y * 4) % 255), 255}
 			w.Set(x, y, rune(1000+(2*(x+(x*y)))+(y+x*y)), grey, purple)
 		}
 	}
 }
 
 func main() {
-	rog.Open(40, 20, "Basic Example", game)
+	rog.Open(48, 32, "Basic Example", game)
 	rog.Start()
 }
