9aa22ab906aae39470188503f915d8428b45985d
Joseph Hager <ajhager@gmail.com>
2012-07-14 15:36:59 -0400

    Add multiple windows to example.

--- examples/main.go
+++ examples/main.go
@@ -5,6 +5,15 @@ import (
 	"github.com/ajhager/rog"
 )
 
+func popup(w *rog.Window) {
+	red := color.RGBA{200, 50, 0, 255}
+	w.SetChFg(2, 5, 'P', red)
+	w.SetChFg(3, 4, 'O', red)
+	w.SetChFg(4, 3, 'P', red)
+	w.SetChFg(5, 4, 'U', red)
+	w.SetChFg(6, 5, 'P', red)
+}
+
 func game(w *rog.Window) {
 	grey := color.RGBA{20, 20, 20, 255}
 	for x := 0; x < w.Width(); x++ {
@@ -16,6 +25,7 @@ func game(w *rog.Window) {
 }
 
 func main() {
+	rog.Open(10, 10, "Popup", popup)
 	rog.Open(48, 32, "Basic Example", game)
 	rog.Start()
 }
