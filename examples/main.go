97343d4775e35b0909d30e5f3b4c5978f3197726
Joseph Hager <ajhager@gmail.com>
2012-07-16 23:25:08 -0400

    Add the start of background color blending modes.

--- examples/main.go
+++ examples/main.go
@@ -7,6 +7,26 @@ import (
 
 func popup(w *rog.Window) {
 	w.Print("fps: %v", w.Fps)
+
+	w.Set(19, 5, -1, nil, color.RGBA{100, 100, 200, 255}, rog.Set)
+	w.Set(20, 5, -1, nil, color.RGBA{100, 100, 200, 255}, rog.Set)
+	w.Set(20, 5, -1, nil, color.RGBA{200, 100, 100, 255}, rog.Set)
+	w.Set(21, 5, -1, nil, color.RGBA{200, 100, 100, 255}, rog.Set)
+
+	w.Set(19, 6, -1, nil, color.RGBA{100, 100, 200, 255}, rog.Set)
+	w.Set(20, 6, -1, nil, color.RGBA{100, 100, 200, 255}, rog.Set)
+	w.Set(20, 6, -1, nil, color.RGBA{200, 100, 100, 255}, rog.Multiply)
+	w.Set(21, 6, -1, nil, color.RGBA{200, 100, 100, 255}, rog.Set)
+
+	w.Set(19, 7, -1, nil, color.RGBA{100, 100, 200, 255}, rog.Set)
+	w.Set(20, 7, -1, nil, color.RGBA{100, 100, 200, 255}, rog.Set)
+	w.Set(20, 7, -1, nil, color.RGBA{200, 100, 100, 255}, rog.Screen)
+	w.Set(21, 7, -1, nil, color.RGBA{200, 100, 100, 255}, rog.Set)
+
+	w.Set(19, 8, -1, nil, color.RGBA{100, 100, 200, 255}, rog.Set)
+	w.Set(20, 8, -1, nil, color.RGBA{100, 100, 200, 255}, rog.Set)
+	w.Set(20, 8, -1, nil, color.RGBA{200, 100, 100, 255}, rog.Overlay)
+	w.Set(21, 8, -1, nil, color.RGBA{200, 100, 100, 255}, rog.Set)
 }
 
 func game(w *rog.Window) {
@@ -14,7 +34,7 @@ func game(w *rog.Window) {
 	for x := 0; x < w.Width(); x++ {
 		for y := 0; y < w.Height(); y++ {
 			purple := color.RGBA{uint8(200), uint8(y * x), uint8((y * 4) % 255), 255}
-			w.Set(x, y, rune(1000+(2*(x+(x*y)))+(y+x*y)), purple, grey)
+			w.Set(x, y, rune(1000+(2*(x+(x*y)))+(y+x*y)), purple, grey, rog.Set)
 		}
 	}
 	w.Print("fps: %v", w.Fps)
