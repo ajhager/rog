2954e603787a5ae2426a3e93675026386a323560
Joseph Hager <ajhager@gmail.com>
2012-07-23 14:10:18 -0400

    Switch from dirty flags to a back buffer.

--- console.go
+++ console.go
@@ -8,59 +8,31 @@ import (
 type Console struct {
 	bg, fg   [][]color.Color
 	ch       [][]rune
-	dirt     [][]bool
 	w, h     int
-	dBg, dFg color.Color
-	blend    ColorBlend
 }
 
 func NewConsole(width, height int) *Console {
 	bg := make([][]color.Color, height)
 	fg := make([][]color.Color, height)
 	ch := make([][]rune, height)
-	dirt := make([][]bool, height)
 
 	for y := 0; y < height; y++ {
 		bg[y] = make([]color.Color, width)
 		fg[y] = make([]color.Color, width)
 		ch[y] = make([]rune, width)
-		dirt[y] = make([]bool, width)
 	}
 
-	con := &Console{bg, fg, ch, dirt, width, height, color.Black, color.White, Normal}
+	con := &Console{bg, fg, ch, width, height}
 
 	for x := 0; x < con.w; x++ {
 		for y := 0; y < con.h; y++ {
 			con.ch[y][x] = ' '
-			con.fg[y][x] = con.dFg
-			con.bg[y][x] = con.dBg
-			con.dirt[y][x] = true
+			con.fg[y][x] = color.White
+			con.bg[y][x] = color.Black
 		}
 	}
 
 	return con
-}
-
-func (con *Console) SetDefaults(fg, bg color.Color, blend ColorBlend) {
-	if fg != nil {
-		con.dFg = fg
-	}
-	if bg != nil {
-		con.dBg = bg
-	}
-	con.blend = blend
-}
-
-func (con *Console) Dirty() {
-	for x := 0; x < con.w; x++ {
-		for y := 0; y < con.h; y++ {
-			con.dirt[y][x] = true
-		}
-	}
-}
-
-func (con *Console) Clear() {
-	con.Fill(0, 0, con.w, con.h, ' ', con.dFg, con.dBg)
 }
 
 func (con *Console) Fill(x0, y0, x1, y1 int, ch rune, fg, bg color.Color, bl ...ColorBlend) {
@@ -72,20 +44,18 @@ func (con *Console) Fill(x0, y0, x1, y1 int, ch rune, fg, bg color.Color, bl ...
 }
 
 func (con *Console) put(x, y int, ch rune, fg, bg color.Color, blend ColorBlend) {
-	if ch > 0 && con.ch[y][x] != ch {
-		con.dirt[y][x] = true
+	if ch > 0 {
 		con.ch[y][x] = ch
 	}
 
-	if fg != nil && !colorEq(fg, con.fg[y][x]) {
-		con.dirt[y][x] = true
+	if fg != nil {
 		con.fg[y][x] = fg
 	}
 
-	if bg != nil && !colorEq(bg, con.bg[y][x]) {
-		con.dirt[y][x] = true
+	if bg != nil {
 		con.bg[y][x] = blend(bg, con.bg[y][x])
-	}
+    }
+
 }
 
 func (con *Console) Set(x, y int, data string, fg, bg color.Color, bl ...ColorBlend) {
