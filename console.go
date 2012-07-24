3a3445526aa48c6189c4c5740542da8f958645a2
Joseph Hager <ajhager@gmail.com>
2012-07-24 14:45:03 -0400

    Added Console.SetR(), which forces wrapping inside a rect bounds.

--- console.go
+++ console.go
@@ -35,14 +35,6 @@ func NewConsole(width, height int) *Console {
 	return con
 }
 
-func (con *Console) Fill(x, y, w, h int, ch rune, fg, bg interface{}) {
-	for i := x; i < x+w; i++ {
-		for j := y; j < y+h; j++ {
-			con.Set(i, j, fg, bg, string(ch))
-		}
-	}
-}
-
 func (con *Console) put(x, y int, fg, bg interface{}, ch rune) {
 	if ch > 0 {
 		con.ch[y][x] = ch
@@ -65,15 +57,42 @@ func (con *Console) put(x, y int, fg, bg interface{}, ch rune) {
     }
 }
 
-func (con *Console) Set(x, y int, fg, bg interface{}, data string, rest ...interface{}) {
+func (con *Console) set(i, j, x, y, w, h int, fg, bg interface{}, data string, rest ...interface{}) {
     runes := []rune(fmt.Sprintf(data, rest...))
     if len(runes) > 0 {
-	    for i := 0; i < len(runes); i++ {
-		    con.put(i+x, y, fg, bg, runes[i])
+        if h == 0 {
+            h = con.h - y
+        }
+	    for k := 0; k < len(runes); k++ {
+            if i == x+w {
+                j += 1
+                i = x
+            }
+            if j == y + h {
+                break
+            }
+		    con.put(i, j, fg, bg, runes[k])
+            i += 1
 	    }
     } else {
-		con.put(x, y, fg, bg, -1)
+		con.put(i, j, fg, bg, -1)
     }
+}
+
+func (con *Console) Set(x, y int, fg, bg interface{}, data string, rest ...interface{}) {
+    con.set(x, y, 0, 0, con.w, con.h, fg, bg, data, rest...)
+}
+
+func (con *Console) SetR(x, y, w, h int, fg, bg interface{}, data string, rest ...interface{}) {
+    con.set(x, y, x, y, w, h, fg, bg, data, rest...)
+}
+
+func (con *Console) Fill(x, y, w, h int, ch rune, fg, bg interface{}) {
+	for i := x; i < x+w; i++ {
+		for j := y; j < y+h; j++ {
+			con.Set(i, j, fg, bg, string(ch))
+		}
+	}
 }
 
 func (con *Console) Width() int {
