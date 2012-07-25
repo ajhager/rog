976e651778993081f0d2931ed77a3051275286b8
Joseph Hager <ajhager@gmail.com>
2012-07-25 15:31:27 -0400

    Split map from fov.

---
+++ map.go
@@ -0,0 +1,87 @@
+package rog
+
+import (
+    "image"
+)
+
+type Map struct {
+	w, h              int
+	blocked, seen [][]bool
+}
+
+func NewMap(width, height int) *Map {
+	blocked := make([][]bool, height)
+	seen := make([][]bool, height)
+
+	for y := 0; y < height; y++ {
+		blocked[y] = make([]bool, width)
+		seen[y] = make([]bool, width)
+	}
+
+	return &Map{width, height, blocked, seen}
+}
+
+// In returns whether the coordinate is inside the map bounds.
+func (this *Map) In(x, y int) bool {
+	return x >= 0 && x < this.w && y >= 0 && y < this.h
+}
+
+// Update runs the give fov alogrithm on the map.
+func (this *Map) Fov(x, y, radius int, includeWalls bool, algo FOVAlgo) {
+	for y := 0; y < this.h; y++ {
+		for x := 0; x < this.w; x++ {
+			this.seen[y][x] = false
+		}
+	}
+	algo(this, x, y, radius, includeWalls)
+}
+
+// Performs astar and returns the list of cells on the path.
+func (this *Map) Path(x0, y0, x1, y1 int) []image.Point {
+    data := NewMapData(this.w, this.h)
+	for y := 0; y < this.h; y++ {
+		for x := 0; x < this.w; x++ {
+            if this.blocked[y][x] {
+			    data[x][y] = WALL
+            } else {
+			    data[x][y] = LAND
+            }
+		}
+	}
+    nodes := Astar(data, x0, y0, x1, y1, true)
+    points := make([]image.Point, len(nodes))
+    for i := 0; i < len(nodes); i++ {
+        points[i] = image.Pt(nodes[i].X, nodes[i].Y)
+    }
+    return points
+}
+
+// Block sets a cell as blocking or not.
+func (this *Map) Block(x, y int, blocked bool) {
+	this.blocked[y][x] = blocked
+}
+
+// Look indicates if the cell at the coordinate can be seen.
+func (this *Map) Look(x, y int) bool {
+	return this.seen[y][x]
+}
+
+// Clear resets the map to completely unblocked but unviewable.
+func (this *Map) Clear() {
+	for y := 0; y < this.h; y++ {
+		for x := 0; x < this.w; x++ {
+			this.blocked[y][y] = false
+			this.seen[y][x] = false
+		}
+	}
+}
+
+// Width returns the width in cells of the map.
+func (this *Map) Width() int {
+	return this.w
+}
+
+// Height returns the height in cells of the map.
+func (this *Map) Height() int {
+	return this.h
+}
