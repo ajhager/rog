d070da1a1cb6b7a1b0638bfb75754b0389bc0113
Joseph Hager <ajhager@gmail.com>
2012-10-06 14:46:33 -0400

    Increased glfw rendering efficiency and reduced memory usage

--- glfw/backend.go
+++ glfw/backend.go
@@ -14,12 +14,20 @@ func init() {
 	rog.SetBackend(new(glfwBackend))
 }
 
+var (
+    vs = []float32{0, 0, 0, 0, 0, 0, 0, 0}
+    cs = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    ts = []float32{0, 0, 0, 0, 0, 0, 0, 0}
+)
+
 type glfwBackend struct {
 	open  bool
 	mouse *rog.MouseData
 	key   int
 	font  image.Image
-	zoom  int
+    s, t float32
+	width, height, zoom int
+    verts []float32
 }
 
 func (w *glfwBackend) Open(width, height, zoom int) {
@@ -28,6 +36,8 @@ func (w *glfwBackend) Open(width, height, zoom int) {
 	}
 
 	w.zoom = zoom
+    w.width = width
+    w.height = height
 
 	glfw.OpenWindowHint(glfw.WindowNoResize, gl.TRUE)
 	err := glfw.OpenWindow(width*16*zoom, height*16*zoom, 8, 8, 8, 8, 0, 0, glfw.Windowed)
@@ -48,6 +58,18 @@ func (w *glfwBackend) Open(width, height, zoom int) {
 		panic(err)
 	}
 	w.font = font
+    w.s = 16 / float32(font.Bounds().Max.X)
+    w.t = 16 / float32(font.Bounds().Max.Y)
+
+	fc := float32(16 * zoom)
+
+	for y := 0; y < height; y++ {
+	    for x := 0; x < width; x++ {
+	        cx := float32(x) * fc
+	        cy := float32(y) * fc
+	        w.verts = append(w.verts, cx, cy, cx, cy + fc, cx + fc, cy + fc, cx + fc, cy)
+		}
+	}
 
 	glInit(width*16*zoom, height*16*zoom, w.font)
 
@@ -80,13 +102,10 @@ func (w *glfwBackend) Render(console *rog.Console) {
 		for y := 0; y < console.Height(); y++ {
 			for x := 0; x < console.Width(); x++ {
 				fg, bg, ch := console.Get(x, y)
-
-	            gl.Color3ub(bg.R, bg.G, bg.B)
-				w.letter(x, y, 0)
+				w.letter(x, y, 0, bg)
 
 				if ch != 0 && ch != 32 {
-	                gl.Color3ub(fg.R, fg.G, fg.B)
-					w.letter(x, y, ch)
+					w.letter(x, y, ch, fg)
 				}
 			}
 		}
@@ -107,6 +126,7 @@ func (w *glfwBackend) mouseMove(x, y int) {
 }
 
 func (w *glfwBackend) mousePress(button, state int) {
+
 	switch state {
 	case glfw.KeyPress:
 		switch button {
@@ -173,25 +193,51 @@ func glInit(width, height int, font image.Image) {
 
 	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, m.Bounds().Max.X, m.Bounds().Max.Y, 0, gl.RGBA, gl.UNSIGNED_BYTE, m.Pix)
 	gl.EnableClientState(gl.VERTEX_ARRAY)
+	gl.EnableClientState(gl.COLOR_ARRAY)
 	gl.EnableClientState(gl.TEXTURE_COORD_ARRAY)
+	gl.VertexPointer(2, 0, vs)
+	gl.ColorPointer(3, 0, cs)
+	gl.TexCoordPointer(2, 0, ts)
 }
 
 // Draw a letter at a certain coordinate
-func (w *glfwBackend) letter(lx, ly int, c rune) {
-	b := w.font.Bounds()
-	fc := float32(16 * w.zoom)
-	cx := float32(lx) * fc
-	cy := float32(ly) * fc
-	verts := []float32{cx, cy, cx, cy + fc, cx + fc, cy + fc, cx + fc, cy, cx, cy}
-	x := float32(c % 256)
-	y := float32(c / 256)
-	s := float32(16) / float32(b.Max.X)
-	t := float32(16) / float32(b.Max.Y)
-	u := x * s
-	v := y * t
-	gl.VertexPointer(2, 0, verts)
-	gl.TexCoordPointer(2, 0, []float32{u, v, u, v + t, u + s, v + t, u + s, v, u, v})
-	gl.DrawArrays(gl.POLYGON, 0, len(verts)/2-1)
+func (w *glfwBackend) letter(lx, ly int, c rune, cl rog.RGB) {
+    start := 8 * (ly*w.width+lx)
+
+    vs[0] = w.verts[start]
+    vs[1] = w.verts[start+1]
+    vs[2] = w.verts[start+2]
+    vs[3] = w.verts[start+3]
+    vs[4] = w.verts[start+4]
+    vs[5] = w.verts[start+5]
+    vs[6] = w.verts[start+6]
+    vs[7] = w.verts[start+7]
+
+    cs[0] = cl.R
+    cs[1] = cl.G
+    cs[2] = cl.B
+    cs[3] = cl.R
+    cs[4] = cl.G
+    cs[5] = cl.B
+    cs[6] = cl.R
+    cs[7] = cl.G
+    cs[8] = cl.B
+    cs[9] = cl.R
+    cs[10] = cl.G
+    cs[11] = cl.B
+
+	u := float32(c % 256) * w.s
+	v := float32(c / 256) * w.t
+    ts[0] = u
+    ts[1] = v
+    ts[2] = u
+    ts[3] = v+w.t
+    ts[4] = u+w.s
+    ts[5] = v+w.t
+    ts[6] = u+w.s
+    ts[7] = v
+
+	gl.DrawArrays(gl.POLYGON, 0, 4)
 }
 
 var glfwToRogKey = map[int]int{
