a5b1c24cd79f2be2f0922587d712509ddcc3395a
Joseph Hager <ajhager@gmail.com>
2012-10-14 09:31:04 -0400

    Remove example from package docs

--- rog.go
+++ rog.go
@@ -4,23 +4,6 @@
 
 /*
 Package rog provides algorithms and data structures for creating roguelike games.
-
- package main
-
- import (
-     "hagerbot.com/rog"
- )
-
- func main() {
-     rog.Open(20, 11, 2, false, "rog", nil)
-     for rog.Running() {
-         rog.Set(5, 5, nil, nil, "Hello, 世界!")
-         if rog.Key() == rog.Esc {
-             rog.Close()
-         }
-         rog.Flush()
-     }
- }
 */
 package rog
 
